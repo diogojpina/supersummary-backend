7c1dc2b9bebe7fc51e4057fcb407e74c400ba95a
Diogo Pina <diogojpina@gmail.com>
2023-08-08 11:33:17 -0300

    Refactoring: Remove movies_controller class variables Remove @ from movies, recommendations, rented because they don't  need to be a class variable since they are not being use in other places than the method context

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
-    render json: @movies
+    movies = Movie.all
+    render json: movies
   end
 
   def recommendations
     favorite_movies = User.find(params[:user_id]).favorites
-    @recommendations = RecommendationEngine.new(favorite_movies).recommendations
-    render json: @recommendations
+    recommendations = RecommendationEngine.new(favorite_movies).recommendations
+    render json: recommendations
   end
 
   def user_rented_movies
-    @rented = User.find(params[:user_id]).rented
-    render json: @rented
+    rented = User.find(params[:user_id]).rented
+    render json: rented
   end
 
   def rent
