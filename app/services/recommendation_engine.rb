76cf883618664a2930b5506f0e5fc27573204c02
Diogo Pina <diogojpina@gmail.com>
2023-08-08 17:20:11 -0300

    Improve Recommendation Engine Get genres using movie ID instead of title, because two movies has same title, it'll fetch both. Besides that, it's faster to select by primary key. Method get_movie_names required a parameter but that parameter it was not used because it reads the favorite_movies from a class variable (filled on the method initialization). Then, for the get_movie_ids, I removed the parameter.

--- app/services/recommendation_engine.rb
+++ app/services/recommendation_engine.rb
@@ -4,20 +4,19 @@ class RecommendationEngine
   end
 
   def recommendations
-    movie_titles = get_movie_names(@favorite_movies)
-    genres = Movie.where(title: movie_titles).pluck(:genre)
+    movie_ids = get_movie_ids()
+    genres = Movie.where(id: movie_ids).pluck(:genre)
     common_genres = genres.group_by{ |e| e }.sort_by{ |k, v| -v.length }.map(&:first).take(3)
     Movie.where(genre: common_genres).order(rating: :desc).limit(10)
   end
 
   private
 
-  def get_movie_names(movies)
-    names = []
+  def get_movie_ids()
+    ids = []
     @favorite_movies.each do |movie|
-      names << movie.title
+      ids << movie.id
     end
-
-    return names
+    return ids
   end
 end
