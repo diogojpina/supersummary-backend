c551f22e6f505466fe18c39f7295920086144baa
Diogo Pina <diogojpina@gmail.com>
2023-08-08 16:08:18 -0300

    Validate input data to movies controller Validate input data for each movies controller method. If validation failed throw http 400 - bad request and a json error message.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,19 +5,45 @@ class MoviesController < ApplicationController
   end
 
   def recommendations
-    favorite_movies = User.find(params[:user_id]).favorites
+    begin
+      user = User.find(params[:user_id])    
+    rescue
+      render :json => { :errors => "User not found! Send a valid user_id." }, :status => 400
+      return
+    end
+
+    favorite_movies = user.favorites
     recommendations = RecommendationEngine.new(favorite_movies).recommendations
     render json: recommendations
   end
 
   def user_rented_movies
-    rented = User.find(params[:user_id]).rented
+    begin
+      user = User.find(params[:user_id])    
+    rescue
+      render :json => { :errors => "User not found! Send a valid user_id." }, :status => 400
+      return
+    end
+
+    rented = user.rented
     render json: rented
   end
 
   def rent
-    user = User.find(params[:user_id])
-    movie = Movie.find(params[:id])
+    begin
+      user = User.find(params[:user_id])    
+    rescue
+      render :json => { :errors => "User not found! Send a valid user_id." }, :status => 400
+      return
+    end
+
+    begin
+      movie = Movie.find(params[:id])
+    rescue
+      render :json => { :errors => "Movie not found! Send a valid movie_id." }, :status => 400
+      return
+    end    
+    
     movie.available_copies -= 1
     movie.save
     user.rented << movie
